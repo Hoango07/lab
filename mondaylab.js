f48c173297c301567e112ef8ad7e20f7791693c2
Hoango07 <91461790+Hoango07@users.noreply.github.com>
2021-10-10 15:49:06 +0700

    Add files via upload

---
+++ mondaylab.js
@@ -0,0 +1,105 @@
+let r = 5;
+let pi = 3.14;
+console.log("EXERCISE1");
+console.log("area of circle: ", r*r*pi);
+
+console.log("END EXERDISE1");
+
+console.log("EXERCISE2");
+
+var a= 10 + 24;
+console.log(a);
+var a= "10" + "24";
+console.log(a)
+var a = "Hello" + " " + 2021;
+console.log(a);
+var a = 1 + 2 * 3;
+console.log(a);
+var a = (1 + 3) **2;
+console.log(a);
+var a = 1/0;
+console.log(a);
+var a = 6%2;
+a = 6/2;
+console.log(a);
+var a = 5.5 % 2;
+console.log(a);
+var a = Number("123")
+console.log(a);
+var a = typeof(1 + "")
+console.log(a);
+var a = 5 == "5"
+console.log(a);
+var a = 5 === "5"
+console.log(a);
+var a = 8!= 8.0
+console.log(a);
+var a = 8 !== 8.0
+console.log(a);
+var a = "true" === true
+console.log(a);
+var a = 4 <= 4.0
+console.log(a);
+var a = 7 >= 7;
+console.log(a);
+var a = true && true
+console.log(a);
+var a = true && false
+console.log(a);
+var a = true || true
+console.log(a);
+var a = false || true
+console.log(a);
+var a = !true
+console.log(a);
+var a = !false
+console.log(a);
+var a = false && (true || true)
+console.log(a);
+var a = false && true || true
+console.log(a);
+
+console.log("END EXERCISE 2")
+
+console.log("EXERCISE3");
+
+var a = 1;
+var b = 4;
+var c = 80;
+
+if (c > b  ) {
+  console.log("biggest number" + [a], [b], [c]);
+} else if (b > a  ) {
+  console.log("Smallest number" + [a], [b] ,[c]);
+} else {
+  console.log("Looking forward to the ...");
+}
+
+console.log("END EXERCISE3");
+
+console.log("EXERCISE4")
+
+var a = 1;
+b = 5;
+for ( i = a; i <= b;i= i+1 ) {
+console.log(i)
+}
+
+console.log("END EXERCISE4")
+
+console.log("EXERCISE5")
+
+// GET LEAP YEAR (1899, 2001);
+
+var leapyear = function leap(y) {
+  var i = 2001;
+  for (let y = 0; y < year.length; y++) {
+    const element = array[y];
+    
+  }
+  if (y % 4 === 0) {
+    console.log(y)
+  }
+  
+}
+console.log("END EXERCISE5")
